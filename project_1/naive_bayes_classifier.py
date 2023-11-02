14a2dec97e467410b9bb3e8fbc7590a6347c1d47
akohnenHeilbronn <akohnen@stud.hs-heilbronn.de>
2023-11-02 11:32:37 +0100

    update

--- project_1/naive_bayes_classifier.py
+++ project_1/naive_bayes_classifier.py
@@ -15,7 +15,6 @@ class NaiveBayes:
         self.feature_probabilities = {}  # the likelihoods
 
         # other variables
-        self.label_column = None
         self.class_labels = []
 
     def fit(self, dataframe: pd.DataFrame, label_column: str):
@@ -49,7 +48,7 @@ class NaiveBayes:
                     df['std'] = dataframe.groupby(by=label_column)[
                         feature_column].std()
                     self.feature_probabilities[feature_column] = df
-                case 'string'|"bool":  # discrete value
+                case 'string' | "bool":  # discrete value
                     grouped = dataframe.groupby(by=feature_column)[
                         label_column]
                     self.feature_probabilities[feature_column] = grouped.value_counts(
@@ -64,6 +63,61 @@ class NaiveBayes:
         :param data: pd.DataFrame to be predicted    X_test
         :return: pd.DataFrame containing probabilities for all categories as well as the classification result
         """
+
+    def predict_probability(self, data: pd.DataFrame):
+        """
+        Calculates the Naive Bayes prediction for a whole pd.DataFrame.
+        :param data: pd.DataFrame to be predicted    X_test
+        :return: pd.DataFrame containing probabilities for all categories as well as the classification result
+        """
+
+        likelyhood_prior = {}
+        prediction_prob = {}
+        prediction_list = []
+
+        # store categorical probabilities across all rows
+        probs = {key: [] for key in self.target_labels}
+
+        for index in data.index:
+            for label in self.target_labels:
+                likelyhood_list = []
+                for column in data.columns:
+                    if data[column].dtypes == float:
+                        std = self.gaus_variables[column][label]["std"]
+                        mean = self.gaus_variables[column][label]["mean"]
+                        likelyhood_list.append(
+                            ((1 / (math.sqrt(2 * math.pi) * std)) * math.exp(-((data[column][index]-mean)**2 / (2 * std**2)))))
+                    else:
+                        feature_label = data[column][index]
+                        likelyhood_list.append(
+                            self.prob_discrete_all[label][column][feature_label])
+
+                # variable änder in zähler
+                likelyhood_prior[label] = math.prod(
+                    likelyhood_list) * self.prior[label]
+
+            evidence = sum(likelyhood_prior.values())
+            for label in self.target_labels:
+                if evidence > float(0):
+                    prediction_prob[label] = likelyhood_prior[label]/evidence
+                else:
+                    prediction_prob[label] = float(0)
+
+            prediction_list.append(
+                max(prediction_prob, key=prediction_prob.get))
+
+            # append current row's categorical probabilities
+            for key in prediction_prob.keys():
+                probs[key].append(prediction_prob[key])
+
+        # create columns with the categorical categories
+        for key in probs.keys():
+            data[key] = probs[key]
+
+        # changed column name to prediction
+        data['prediction'] = prediction_list
+
+        return data
 
     def evaluate_on_data(self, data: pd.DataFrame, test_labels):
         """
